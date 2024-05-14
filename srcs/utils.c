23923a0eefef2ad8d6929c4272571645a985e179
Timberly Ng <ting@c1r8s12.42singapore.sg>
2024-05-14 21:31:27 +0800

    experimenting with fork and pipe

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 11:55:01 by ting              #+#    #+#             */
-/*   Updated: 2024/05/14 21:10:52 by ting             ###   ########.fr       */
+/*   Updated: 2024/05/14 21:31:16 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ char    *find_path(char *cmd, char **env)
     path_in_env = ft_strnstr();
     while ((path_in_env = ft_strnstr(env[i], "PATH", 4)) == 0)
         i++;
+
+    //add the command behind the path to try it with access; etc bin/ls to try for ls
     
 }
